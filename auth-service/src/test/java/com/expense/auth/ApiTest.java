46de08e1e10747e993d70c61cc8e341da174b855
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2025-03-08 16:18:24 +0330

    Fix early failed test and complete the getUserByUsername API testing

--- auth-service/src/test/java/com/expense/auth/ApiTest.java
+++ auth-service/src/test/java/com/expense/auth/ApiTest.java
@@ -1,15 +1,17 @@
 package com.expense.auth;
 
-import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApiTest {
@@ -21,10 +23,34 @@ public class ApiTest {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${test.username}")
+    private String username;
+
+    @Value("${test.password}")
+    private String password;
+
+    private AccessTokenResponse tokenResponse;
+
+    @BeforeEach
+    void setUp() {
+        // No need to use RestTemplate for getting token from Keycloak
+        // Use AuthzClient API from Keycloak community
+        this.tokenResponse = AuthzClient.create().obtainAccessToken(username, password);
+    }
+
+    /**
+     * Test getting user details from /api/users/{username}
+     * Nullity check, Content Check and ResponseStatus Check
+     */
     @Test
     public void testGettingUserFromKeycloakByUsername() {
-        ResponseEntity<List> entity = this.restTemplate.getForEntity(
-                BASE_URL.formatted(port, "/users/testuser"), List.class);
-        BDDAssertions.then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(tokenResponse.getToken());
+        ResponseEntity<UserRepresentation> entity = restTemplate.exchange(
+                BASE_URL.formatted(port, "users/%s".formatted(username)),
+                HttpMethod.GET, new HttpEntity<>(headers), UserRepresentation.class);
+        Assertions.assertNotNull(entity.getBody());
+        Assertions.assertEquals(username, entity.getBody().getUsername());
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
 }
